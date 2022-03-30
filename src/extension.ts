0f8ca5a95652267d63d876fdc0b84df4a5d18f98
vitorva <vitor.vazafonso@gmail.com>
2022-03-30 14:43:30 +0200

    completion and hover base

--- src/extension.ts
+++ src/extension.ts
@@ -12,6 +12,91 @@ export function activate(context: vscode.ExtensionContext) {
             new UonConfigDocumentSymbolProvider()
         )
     );
+
+
+	const provider1 = vscode.languages.registerCompletionItemProvider({scheme: "file", language: "uon"}, {
+
+		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+
+			// a simple completion item which inserts `Hello World!`
+			const simpleCompletion = new vscode.CompletionItem('Hello World!');
+
+			// a completion item that inserts its text as snippet,
+			// the `insertText`-property is a `SnippetString` which will be
+			// honored by the editor.
+			const snippetCompletion = new vscode.CompletionItem('Good part of the day');
+			snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
+			const docs : any = new vscode.MarkdownString("Inserts a snippet that lets you select [link](x.ts).");
+			snippetCompletion.documentation = docs;
+			docs.baseUri = vscode.Uri.parse('http://example.com/a/b/c/');
+
+			// a completion item that can be accepted by a commit character,
+			// the `commitCharacters`-property is set which means that the completion will
+			// be inserted and then the character will be typed.
+			const commitCharacterCompletion = new vscode.CompletionItem('console');
+			commitCharacterCompletion.commitCharacters = ['.'];
+			commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
+
+			// a completion item that retriggers IntelliSense when being accepted,
+			// the `command`-property is set which the editor will execute after 
+			// completion has been inserted. Also, the `insertText` is set so that 
+			// a space is inserted after `new`
+			const commandCompletion = new vscode.CompletionItem('new');
+			commandCompletion.kind = vscode.CompletionItemKind.Keyword;
+			commandCompletion.insertText = 'new ';
+			commandCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
+
+			// return all completion items as array
+			return [
+				simpleCompletion,
+				snippetCompletion,
+				commitCharacterCompletion,
+				commandCompletion
+			];
+		}
+	});
+
+    const provider2 = vscode.languages.registerCompletionItemProvider(
+		'plaintext',
+		{
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+
+				// get all text until the `position` and check if it reads `console.`
+				// and if so then complete if `log`, `warn`, and `error`
+				const linePrefix = document.lineAt(position).text.substr(0, position.character);
+				if (!linePrefix.endsWith('console.')) {
+					return undefined;
+				}
+
+				return [
+					new vscode.CompletionItem('log', vscode.CompletionItemKind.Method),
+					new vscode.CompletionItem('warn', vscode.CompletionItemKind.Method),
+					new vscode.CompletionItem('error', vscode.CompletionItemKind.Method),
+				];
+			}
+		},
+		'.' // triggered whenever a '.' is being typed
+	);
+
+	context.subscriptions.push(provider1, provider2);
+
+    const hover = vscode.languages.registerHoverProvider({scheme: "file", language: "uon"}, {
+        provideHover(document, position, token) {
+
+            const range = document.getWordRangeAtPosition(position);
+            const word = document.getText(range);
+
+            if (word == "HELLO") {
+
+                return new vscode.Hover({
+                    language: "Hello language",
+                    value: "Hello Value"
+                });
+            }
+        }
+    });
+
+    context.subscriptions.push(hover)
 }
 
 class UonConfigDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
@@ -41,7 +126,7 @@ class UonConfigDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
 
                 let tokens = line.text.split(" ")
 
-                if (line.text.startsWith("#BEGIN_COMP")) {
+                if (line.text.startsWith("#BEGIN")) {
 					console.log("line.range", line.range)
 
                     let marker_symbol = new vscode.DocumentSymbol(
@@ -58,7 +143,7 @@ class UonConfigDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
                     }
                     // marker_symbol.children.push(_boot)
                 }
-                else if (line.text.startsWith("#END_COMP")) {
+                else if (line.text.startsWith("#END")) {
                     // TODO check if nodes has length 1 before popping.
                     if (inside_marker) {
                         nodes.pop()
