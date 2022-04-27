6e91acc67684b31c84a22110bc936682f66f7395
vitorva <vitor.vazafonso@gmail.com>
2022-04-27 11:24:48 +0200

    clean up

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,16 @@
 'use strict';
 
-// src/extension.ts
-
 import * as vscode from 'vscode';
-
-import * as fs from "fs";
-import * as path from "path";
 
 import { UONLexer } from './generated/UONLexer';
 import { UONParser } from "./generated/UONParser";
 
-import { CompletionItem, CompletionItemKind, InsertTextFormat } from 'vscode-languageserver';
 
 import {
-    ANTLRErrorListener, CharStreams, CommonToken, CommonTokenStream, RecognitionException, Recognizer, Token, TokenStream
+    CharStreams, CommonTokenStream, TokenStream
 } from "antlr4ts";
 
 import * as c3 from 'antlr4-c3';
-import { ScopedSymbol, Symbol } from 'antlr4-c3';
 
 const possibleIdentifierPrefix = /[\w]$/;
 const lineSeparator = /\n|\r|\r\n/g;
@@ -56,58 +49,12 @@ export function findCursorTokenIndex(tokenStream: TokenStream, cursor: CursorPos
 
 export function activate(context: vscode.ExtensionContext) {
 
-    context.subscriptions.push(
-        vscode.languages.registerDocumentSymbolProvider(
-            {scheme: "file", language: "uon"}, 
-            new UonConfigDocumentSymbolProvider()
-        )
-    );
-
 	const provider1 = vscode.languages.registerCompletionItemProvider({scheme: "file", language: "uon"}, {
 
 		async provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
 
-            /*
-			// a simple completion item which inserts `Hello World!`
-			const simpleCompletion = new vscode.CompletionItem('Hello World!');
-
-			// a completion item that inserts its text as snippet,
-			// the `insertText`-property is a `SnippetString` which will be
-			// honored by the editor.
-			const snippetCompletion = new vscode.CompletionItem('Good part of the day');
-			snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
-			const docs : any = new vscode.MarkdownString("Inserts a snippet that lets you select [link](x.ts).");
-			snippetCompletion.documentation = docs;
-			docs.baseUri = vscode.Uri.parse('http://example.com/a/b/c/');
-
-			// a completion item that can be accepted by a commit character,
-			// the `commitCharacters`-property is set which means that the completion will
-			// be inserted and then the character will be typed.
-			const commitCharacterCompletion = new vscode.CompletionItem('console');
-			commitCharacterCompletion.commitCharacters = ['.'];
-			commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
-
-			// a completion item that retriggers IntelliSense when being accepted,
-			// the `command`-property is set which the editor will execute after 
-			// completion has been inserted. Also, the `insertText` is set so that 
-			// a space is inserted after `new`
-			const commandCompletion = new vscode.CompletionItem('new');
-			commandCompletion.kind = vscode.CompletionItemKind.Keyword;
-			commandCompletion.insertText = 'new ';
-			commandCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
-
-			// return all completion items as array
-			return [
-				simpleCompletion,
-				snippetCompletion,
-				commitCharacterCompletion,
-				commandCompletion
-			];
-            */
-
             //Chaine de texte de l'éditeur
             const inputStream = CharStreams.fromString(document.getText());
-
 
             const splitted = document.getText().split(/(\s+)/);
             console.log("splitted", splitted);
@@ -115,7 +62,6 @@ export function activate(context: vscode.ExtensionContext) {
             const lexer = new UONLexer(inputStream);
             const tokenStream = new CommonTokenStream(lexer);
             
-
             const parser = new UONParser(tokenStream);
             let tree = parser.uon();
 
@@ -155,14 +101,8 @@ export function activate(context: vscode.ExtensionContext) {
             console.log("splitted.length", splitted.length -1);
             let candidates = core.collectCandidates(splitted.length -1);
 
-
             let keywords : vscode.CompletionItem[] = [];
             for (let candidate of candidates.tokens) {
-                    
-                //for (let moreCandidates of candidate[1]) {
-                //    keywords.push(new vscode.CompletionItem(parser.vocabulary.getDisplayName(moreCandidates), vscode.CompletionItemKind.Keyword));
-                //}
-
                 let str = parser.vocabulary.getDisplayName(candidate[0]);
 
                 //https://stackoverflow.com/questions/19156148/i-want-to-remove-double-quotes-from-a-string
@@ -171,52 +111,12 @@ export function activate(context: vscode.ExtensionContext) {
                 keywords.push( new vscode.CompletionItem(str, vscode.CompletionItemKind.Keyword));
             }
 
-    
-            //const symbolTable = new c3.SymbolTable("test", { allowDuplicateSymbols: false });
-            //const lol = symbolTable.getSymbolsOfType(c3.Symbol);
-
-            /*
-            for (let candidate of candidates.rules) {
-                switch (candidate[0]) {
-                  case UONParser.RULE_obj: {
-                      console.log("SUPER", parser.vocabulary.getDisplayName(candidate[0]));
-                    
-                    let variables = await symbolTable.getSymbolsOfType(c3.VariableSymbol);
-                    for (let variable of variables)
-                      {keywords.push(new vscode.CompletionItem(variable.name, vscode.CompletionItemKind.Keyword));}
-                    break;
-                    
-                  }
-                }
-              }
-            */
             return keywords;
 		}
 	}, "!", ":"," ");
 
-    const provider2 = vscode.languages.registerCompletionItemProvider(
-		'plaintext',
-		{
-			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
 
-				// get all text until the `position` and check if it reads `console.`
-				// and if so then complete if `log`, `warn`, and `error`
-				const linePrefix = document.lineAt(position).text.substr(0, position.character);
-				if (!linePrefix.endsWith('console.')) {
-					return undefined;
-				}
-
-				return [
-					new vscode.CompletionItem('log', vscode.CompletionItemKind.Method),
-					new vscode.CompletionItem('warn', vscode.CompletionItemKind.Method),
-					new vscode.CompletionItem('error', vscode.CompletionItemKind.Method),
-				];
-			}
-		},
-		'.' // triggered whenever a '.' is being typed
-	);
-
-	context.subscriptions.push(provider1, provider2);
+	context.subscriptions.push(provider1);
 
     const hover = vscode.languages.registerHoverProvider({scheme: "file", language: "uon"}, {
         provideHover(document, position, token) {
@@ -224,7 +124,7 @@ export function activate(context: vscode.ExtensionContext) {
             const range = document.getWordRangeAtPosition(position);
             const word = document.getText(range);
 
-            if (word == "!str") {
+            if (word === "!str") {
 
                 return new vscode.Hover({
                     language: "String encoded in UTF-8",
@@ -234,113 +134,5 @@ export function activate(context: vscode.ExtensionContext) {
         }
     });
 
-    context.subscriptions.push(hover)
-}
-
-class UonConfigDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
-
-    private format(cmd: string):string{
-        return cmd.substr(1).toLowerCase().replace(/^\w/, c => c.toUpperCase())
-    }
-
-    public provideDocumentSymbols(
-        document: vscode.TextDocument,
-        token: vscode.CancellationToken): Promise<vscode.DocumentSymbol[]> 
-        {
-        return new Promise((resolve, reject) => 
-        {
-            let symbols: vscode.DocumentSymbol[] = [];
-            let nodes = [symbols]
-            let inside_marker = false
-            let inside_run = false
-            let inside_userinput = false
-
-            let symbolkind_marker = vscode.SymbolKind.Field
-            let symbolkind_run = vscode.SymbolKind.Event
-            let symbolkind_cmd = vscode.SymbolKind.Function
-
-            for (var i = 0; i < document.lineCount; i++) {
-                var line = document.lineAt(i);
-
-                let tokens = line.text.split(" ")
-
-                if (line.text.startsWith("{")) {
-					console.log("line.range", line.range)
-
-                    let marker_symbol = new vscode.DocumentSymbol(
-                        this.format(tokens[0]) + " " + tokens[1],
-                        'Component',
-                        symbolkind_marker,
-                        line.range, line.range)
-
-
-                    nodes[nodes.length-1].push(marker_symbol)
-                    if (!inside_marker) {
-                        nodes.push(marker_symbol.children)
-                        inside_marker = true
-                    }
-                    // marker_symbol.children.push(_boot)
-                }
-                else if (line.text.startsWith("}")) {
-                    // TODO check if nodes has length 1 before popping.
-                    if (inside_marker) {
-                        nodes.pop()
-                        inside_marker = false
-                    }
-                }
-                else if (line.text.startsWith("#RUN") || line.text.startsWith("#END")) {
-                    
-                    let run_symbol = new vscode.DocumentSymbol(
-                        this.format(tokens[0]),
-                        'Session separator',
-                        symbolkind_run,
-                        line.range, line.range)
-
-                    if (inside_run) {
-                        nodes.pop()
-                    }
-    
-                    nodes[nodes.length-1].push(run_symbol)
-                    nodes.push(run_symbol.children)
-                    inside_run = true
-                }
-                else if (line.text.startsWith("#USERINPUTBEGIN")) {
-
-                    let user_symbol = new vscode.DocumentSymbol(
-                        this.format(tokens[0]),
-                        'User module',
-                        vscode.SymbolKind.Interface,
-                        line.range, line.range)
-
-
-                    nodes[nodes.length-1].push(user_symbol)
-                    if (!inside_userinput) {
-                        nodes.push(user_symbol.children)
-                        inside_userinput = true
-                    }
-                    // marker_symbol.children.push(_boot)
-                }
-                else if (line.text.startsWith("#USERINPUTEND")) {
-                    // TODO check if nodes has length 1 before popping.
-                    if (inside_userinput) {
-                        nodes.pop()
-                        inside_userinput = false
-                    }
-                }                
-                else if (line.text.startsWith("#")) {
-					
-
-                    let cmd_symbol = new vscode.DocumentSymbol(
-                        this.format(tokens[0]),
-                        '',
-                        symbolkind_cmd,
-                        line.range, line.range)
-
-                    nodes[nodes.length-1].push(cmd_symbol)
-                }
-            }
-
-            resolve(symbols);
-        });
-    }
+    context.subscriptions.push(hover);
 }
