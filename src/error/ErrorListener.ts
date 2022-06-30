6a4290961bb0194413109f9537ccf1560782d85e
vitorva <vitor.vazafonso@gmail.com>
2022-06-30 14:35:17 +0200

    refactoring

---
+++ src/error/ErrorListener.ts
@@ -0,0 +1,82 @@
+import * as vscode from 'vscode';
+import { ANTLRErrorListener, CommonToken, RecognitionException, Recognizer, Token } from "antlr4ts";
+import { UONParser } from '../generated/UONParser';
+
+export class ErrorListener implements ANTLRErrorListener<CommonToken> {
+
+    level = 0;
+    document: vscode.TextDocument;
+    collection: vscode.DiagnosticCollection;
+    context: vscode.ExtensionContext; // vscode.ExtensionContext !!!! ???
+    parser: UONParser;
+
+    constructor(document: vscode.TextDocument, collection: vscode.DiagnosticCollection, context: vscode.ExtensionContext, parser: UONParser) {
+        this.document = document;
+        this.collection = collection;
+        this.context = context;
+        this.parser = parser;
+    }
+
+    public updateDiagnostics(document: vscode.TextDocument, collection: vscode.DiagnosticCollection, msg: string, range: vscode.Range): void {
+        if (document) {
+            collection.set(document.uri, [{
+                code: '',
+                message: msg,
+                range: range,
+                severity: vscode.DiagnosticSeverity.Error,
+                source: '',
+                relatedInformation: [
+                    new vscode.DiagnosticRelatedInformation(new vscode.Location(document.uri, range), "")
+                ]
+            }]);
+        } else {
+            collection.clear();
+        }
+    }
+
+    public errorCount = 0;
+
+    public syntaxError<T extends Token>(recognizer: Recognizer<T, any>, offendingSymbol: T | undefined, line: number,
+        charPositionInLine: number, msg: string, e: RecognitionException | undefined): void {
+        // ANTLRErrorListener -> ANTLRErrorStrategy -> DefaultErrorStrategy.ts 
+        //  - notifyErrorListeners
+        //  - e : RecognitionException
+        //  - IntervalSet
+        // Lien entre syntaxError et notifyErrorListeners ???
+        const expectedTokensString = e?.expectedTokens?.toStringVocabulary(recognizer.vocabulary);
+        const expectedTokens = e?.expectedTokens?.toArray();
+
+        if (expectedTokensString === undefined) {
+            //return
+        }
+        console.log(e?.message);
+
+
+        if (expectedTokens !== undefined) {
+            //return
+            for (let index = 0; index < expectedTokens.length; index++) {
+                const element = expectedTokens[index];
+                console.log(this.parser.vocabulary.getDisplayName(element));
+
+            }
+        } else {
+            // TODO
+        }
+
+        //console.log(this.parser.vocabulary.getDisplayName(expectedTokens[0]));
+
+        if (this.errorCount > 0) {
+            return
+        }
+        ++this.errorCount;
+
+        console.log("ERROR", line + "-" + charPositionInLine + " : " + msg);
+
+        console.log("charPositionInLine", charPositionInLine);
+
+        if (vscode.window.activeTextEditor) {
+            const range = new vscode.Range(new vscode.Position((line - 1), charPositionInLine), new vscode.Position((line - 1), charPositionInLine));
+            this.updateDiagnostics(vscode.window.activeTextEditor.document, this.collection, msg, range);
+        }
+    }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import * as vscode from 'vscode';
 import { CompletionItem, CompletionItemKind, InsertTextFormat } from 'vscode-languageserver';
 
 import {
-  ANTLRErrorListener, CharStreams, CommonToken, CommonTokenStream, RecognitionException, Recognizer, Token
+  CharStreams, CommonTokenStream
 } from "antlr4ts";
 
 import * as c3 from 'antlr4-c3';
@@ -14,88 +14,9 @@ import { UONLexer } from './generated/UONLexer';
 import { UONParser } from "./generated/UONParser";
 import { UonCompletionErrorStrategy } from './error/UonCompletionErrorStrategy';
 import { UonASTVisitor } from './outline/UonASTVisitor';
+import { ErrorListener } from './error/ErrorListener';
 
 const hoverJson = require("./hover.json");
-
-export class ErrorListener implements ANTLRErrorListener<CommonToken> {
-
-  level = 0;
-  document: vscode.TextDocument;
-  collection: vscode.DiagnosticCollection;
-  context: vscode.ExtensionContext; // vscode.ExtensionContext !!!! ???
-  parser: UONParser;
-
-  constructor(document: vscode.TextDocument, collection: vscode.DiagnosticCollection, context: vscode.ExtensionContext, parser: UONParser) {
-    this.document = document;
-    this.collection = collection;
-    this.context = context;
-    this.parser = parser;
-  }
-
-  public updateDiagnostics(document: vscode.TextDocument, collection: vscode.DiagnosticCollection, msg: string, range: vscode.Range): void {
-    if (document) {
-      collection.set(document.uri, [{
-        code: '',
-        message: msg,
-        range: range,
-        severity: vscode.DiagnosticSeverity.Error,
-        source: '',
-        relatedInformation: [
-          new vscode.DiagnosticRelatedInformation(new vscode.Location(document.uri, range), "")
-        ]
-      }]);
-    } else {
-      collection.clear();
-    }
-  }
-
-  public errorCount = 0;
-
-  public syntaxError<T extends Token>(recognizer: Recognizer<T, any>, offendingSymbol: T | undefined, line: number,
-    charPositionInLine: number, msg: string, e: RecognitionException | undefined): void {
-    // ANTLRErrorListener -> ANTLRErrorStrategy -> DefaultErrorStrategy.ts 
-    //  - notifyErrorListeners
-    //  - e : RecognitionException
-    //  - IntervalSet
-    // Lien entre syntaxError et notifyErrorListeners ???
-    const expectedTokensString = e?.expectedTokens?.toStringVocabulary(recognizer.vocabulary);
-    const expectedTokens = e?.expectedTokens?.toArray();
-
-    if (expectedTokensString === undefined) {
-      //return
-    }
-    console.log(e?.message);
-
-
-    if (expectedTokens !== undefined) {
-      //return
-      for (let index = 0; index < expectedTokens.length; index++) {
-        const element = expectedTokens[index];
-        console.log(this.parser.vocabulary.getDisplayName(element));
-
-      }
-    } else {
-      // TODO
-    }
-
-
-    //console.log(this.parser.vocabulary.getDisplayName(expectedTokens[0]));
-
-    if (this.errorCount > 0) {
-      return
-    }
-    ++this.errorCount;
-
-    console.log("ERROR", line + "-" + charPositionInLine + " : " + msg);
-
-    console.log("charPositionInLine", charPositionInLine);
-
-    if (vscode.window.activeTextEditor) {
-      const range = new vscode.Range(new vscode.Position((line - 1), charPositionInLine), new vscode.Position((line - 1), charPositionInLine));
-      this.updateDiagnostics(vscode.window.activeTextEditor.document, this.collection, msg, range);
-    }
-  }
-}
 
 export function activate(context1: vscode.ExtensionContext) {
 
