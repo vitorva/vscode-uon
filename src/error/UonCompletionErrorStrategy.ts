3a08a79fe14bfc6101c162b2e4cd33ddca7eef11
vitorva <vitor.vazafonso@gmail.com>
2022-07-25 10:38:41 +0200

    clean up UonCompletionErrorStrategy

--- src/error/UonCompletionErrorStrategy.ts
+++ src/error/UonCompletionErrorStrategy.ts
@@ -1,6 +1,5 @@
 import { DefaultErrorStrategy, Parser, RecognitionException, Token } from "antlr4ts";
 import { IntervalSet } from "antlr4ts/misc/IntervalSet";
-import { UONParser } from "../generated/UONParser";
 
 export class UonCompletionErrorStrategy extends DefaultErrorStrategy {
   protected singleTokenDeletion(recognizer: Parser): Token | undefined {
@@ -17,12 +16,6 @@ export class UonCompletionErrorStrategy extends DefaultErrorStrategy {
 
   protected getErrorRecoverySet(recognizer: Parser): IntervalSet {
     const defaultRecoverySet = super.getErrorRecoverySet(recognizer);
-    if (recognizer.ruleContext.ruleIndex === UONParser.RULE_presentation_properties) {
-      const uonFollowSet = new IntervalSet();
-      uonFollowSet.add(UONParser.DESCRIPTION);
-      uonFollowSet.add(UONParser.OPTIONAL);
-      //if (uonFollowSet.size > 0) { return uonFollowSet; };
-    }
     return defaultRecoverySet;
   }
 }
