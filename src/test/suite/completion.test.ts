61d7c74cb26fecef6402f39e0de6828b251847ac
vitorva <vitor.vazafonso@gmail.com>
2022-07-27 14:48:33 +0200

    tests

--- src/test/suite/completion.test.ts
+++ src/test/suite/completion.test.ts
@@ -8,8 +8,6 @@ import {
     ANTLRErrorListener, CharStreams, CommonToken, CommonTokenStream, RecognitionException, Recognizer, Token,
 } from "antlr4ts";
 
-// Some helper functions + types to create certain setups.
-
 export class ErrorListener implements ANTLRErrorListener<CommonToken> {
     public errorCount = 0;
 
@@ -23,34 +21,66 @@ export class ErrorListener implements ANTLRErrorListener<CommonToken> {
 suite("antlr4-c3", function () {
     this.slow(1000);
 
-    test("Uon parser", () => {
-            const inputStream = CharStreams.fromString('!map {key : "value"}');
-            const lexer = new UONLexer(inputStream);
-            const tokenStream = new CommonTokenStream(lexer);
+    test("Test completion 1", () => {
+        const inputStream = CharStreams.fromString('!map {key : "value"}');
+        const lexer = new UONLexer(inputStream);
+        const tokenStream = new CommonTokenStream(lexer);
 
-            const parser = new UONParser(tokenStream);
-            const errorListener = new ErrorListener();
-            parser.addErrorListener(errorListener);
-            parser.uon();
-            expect(errorListener.errorCount, "Test 1").equals(0);
+        const parser = new UONParser(tokenStream);
+        const errorListener = new ErrorListener();
+        parser.addErrorListener(errorListener);
+        parser.uon();
+        expect(errorListener.errorCount, "Test 1").equals(0);
 
-            const core = new c3.CodeCompletionCore(parser);
-            core.ignoredTokens = new Set([
-                UONLexer.OPEN_C_BRA,
-                UONLexer.CLOSE_C_BRA,
-                UONLexer.OPEN_S_BRA,
-                UONLexer.CLOSE_S_BRA,
-                UONLexer.OPEN_PAR,
-                UONLexer.CLOSE_PAR,
-                UONLexer.COMMA,
-                UONLexer.COLON,
-                UONLexer.QUOTED_STRING,
-                UONLexer.UNQUOTED_STRING
-            ]);
+        const core = new c3.CodeCompletionCore(parser);
+        core.ignoredTokens = new Set([
+            UONLexer.OPEN_C_BRA,
+            UONLexer.CLOSE_C_BRA,
+            UONLexer.OPEN_S_BRA,
+            UONLexer.CLOSE_S_BRA,
+            UONLexer.OPEN_PAR,
+            UONLexer.CLOSE_PAR,
+            UONLexer.COMMA,
+            UONLexer.COLON,
+            UONLexer.QUOTED_STRING,
+            UONLexer.UNQUOTED_STRING
+        ]);
 
-            // 1) At the input start.
-            let candidates = core.collectCandidates(0);
-            expect(candidates.tokens.size, "Test 2").to.equal(29);
-            expect(candidates.tokens.has(UONLexer.MAPPING_TYPE), "Test 3").to.equal(true);
+        // 1) At the input start.
+        let candidates = core.collectCandidates(0);
+        expect(candidates.tokens.size, "Test 2").to.equal(29);
+        expect(candidates.tokens.has(UONLexer.MAPPING_TYPE), "Test 3").to.equal(true);
+
+        candidates = core.collectCandidates(7);
+        expect(candidates.tokens.size, "Test 4").to.equal(42);
+    });
+
+    test("Test completion 2", () => {
+        const inputStream = CharStreams.fromString('{ motor : {weight : !int( unit : 12) 12}}');
+        const lexer = new UONLexer(inputStream);
+        const tokenStream = new CommonTokenStream(lexer);
+
+        const parser = new UONParser(tokenStream);
+        const errorListener = new ErrorListener();
+        parser.addErrorListener(errorListener);
+        parser.uon();
+        expect(errorListener.errorCount, "Test 1").equals(0);
+
+        const core = new c3.CodeCompletionCore(parser);
+        core.ignoredTokens = new Set([
+            UONLexer.OPEN_C_BRA,
+            UONLexer.CLOSE_C_BRA,
+            UONLexer.OPEN_S_BRA,
+            UONLexer.CLOSE_S_BRA,
+            UONLexer.OPEN_PAR,
+            UONLexer.CLOSE_PAR,
+            UONLexer.COMMA,
+            UONLexer.COLON,
+            UONLexer.QUOTED_STRING,
+            UONLexer.UNQUOTED_STRING
+        ]);
+
+        let candidates = core.collectCandidates(18);
+        expect(candidates.tokens.size, "Test 2").to.equal(7);
     });
 });
